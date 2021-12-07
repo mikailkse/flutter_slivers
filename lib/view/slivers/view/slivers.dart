1518ba5a2fbd81900329251b3835c2baf69a396b
mikail <41960652+nerdropss@users.noreply.github.com>
2021-12-07 12:40:09 +0300

    update

--- lib/view/slivers/view/slivers.dart
+++ lib/view/slivers/view/slivers.dart
@@ -28,6 +28,7 @@ class _SliversState extends State<Slivers> {
       body: Scaffold(
         body: CustomScrollView(
           slivers: [
+            textSliverToBoxAdapter(context),
             SliverListCard(items: _items),
             SliverGrid(
               gridDelegate: HomePageGridDelegate(),
@@ -65,6 +66,20 @@ class _SliversState extends State<Slivers> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter textSliverToBoxAdapter(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Text(
+        "Rick And Morty",
+        style: Theme.of(context)
+            .textTheme
+            .headline4!
+            .copyWith(fontWeight: FontWeight.bold)
+            .copyWith(color: Colors.teal),
+        textAlign: TextAlign.center,
       ),
     );
   }
